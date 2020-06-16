6b54901822ee25531b23dce35ee9b58895b2aafe
Sebastien REMY <seb.remy@me.com>
2020-06-16 08:45:14 +0200

    Enabling high-performance Metal rendering with drawingGroup()

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -10,16 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
-    var body: some View {
-//        Text("Hello World")
-//        .frame(width: 300, height: 300)
-//         .border(ImagePaint(image: Image("Example"), scale: 0.1), width: 30)
+    struct ColorCyclingCircle: View {
+        var amount = 0.0
+        var steps = 100
+
+        @State private var colorCycle = 0.0
         
-        Capsule()
-        .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
-        .frame(width: 300, height: 200)
+        var body: some View {
+            ZStack {
+                ForEach(0..<steps) { value in
+                    Circle()
+                        .inset(by: CGFloat(value))
+//                        .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+                }
+            }
+            // Accelerate drawing with drawing group
+            .drawingGroup()
+        }
+
+        func color(for value: Int, brightness: Double) -> Color {
+            var targetHue = Double(value) / Double(self.steps) + self.amount
+
+            if targetHue > 1 {
+                targetHue -= 1
+            }
+
+            return Color(hue: targetHue, saturation: 1, brightness: brightness)
+        }
     }
+    
+    @State private var colorCycle = 0.0
+
+   var body: some View {
+       VStack {
+           ColorCyclingCircle(amount: self.colorCycle)
+               .frame(width: 300, height: 300)
+
+           Slider(value: $colorCycle)
+       }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
